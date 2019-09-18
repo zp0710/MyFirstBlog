e930ca0eaa1da205bf0b8a3efdaa0fbd716cd54c
zp0710 <45162023+zp0710@users.noreply.github.com>
2019-09-18 23:27:54 +0800

    Add files via upload

---
+++ AdminAuthor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstBlog.Models
+{
+    public class AdminAuthor: AuthorizeAttribute
+    {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+
+            if (httpContext.Session["Adminvalid"]!=null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            try
+            {
+                filterContext.HttpContext.Response.Redirect("/Login/Login");
+            }
+            catch(Exception)
+            {
+                return;
+            }
+        }
+    }
+}
---
+++ AuthorizationFilter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace MyFirstBlog.Models
+{
+    //实现了授权过滤的特性类
+    public class AuthorizationFilter:AuthorizeAttribute
+    {
+        //此方法逻辑处理，在此处为判断用户是否登录
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            if (httpContext.Session["valid"] != null)
+                return true;
+         
+                else
+
+                return false;
+          
+        }
+
+        //此方法为没有取得授权所执行的操作
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            //ContentResult contentResult = new ContentResult();
+            //contentResult.Content = "meiyoudenglu";
+            //filterContext.Result = contentResult;
+
+            try
+            {
+                filterContext.HttpContext.Response.Redirect("/Login/Login");
+               
+            }
+            catch (Exception)
+            {
+
+                return;
+            }
+         
+        }
+    }
+}
